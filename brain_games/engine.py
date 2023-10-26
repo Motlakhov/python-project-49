3b0885381e3ac03478b120e223c4cc697cebf1cf
motlakhov <dmitrijmotlahov@gmail.com>
2023-10-26 17:32:18 +0300

    step 6

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -9,12 +9,14 @@ def run_game(game):
     print(game.RULES)
     win_score = 0
     while win_score < ROUNDS:
-        correct_answer, question = game.task_func()
+        question, correct_answer = game.task_func()
+        print(f'Question: {question}')
         answer = prompt.string('Your answer: ')
         if answer == correct_answer:
             print('Correct!')
             win_score += 1
         elif answer != correct_answer:
-            print(f"{answer} is wrong answer ;(. Correct answer was {correct_answer}.\nLet's try again, {name}!")
-        if win_score == finish:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.\nLet's try again, {name}!")
+            break
+        if win_score == ROUNDS:
             print(f'Congratulations, {name}!')
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,29 +1,11 @@
 #!/usr/bin/env python3
 
-import prompt
-from random import randint
+from brain_games.engine import run_game
+from brain_games.games import even
 
 
 def main():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    print(f'Answer "yes" if the number is even, otherwise answer "no"')
-    score = 0
-    finish = 3
-    while score < finish:
-        random_number = randint(1,100)
-        print(f'Question:{random_number}')
-        user_answer = prompt.string('Your answer: ')
-        correct_answer = 'yes' if random_number % 2 == 0 else 'no'
-        if user_answer == correct_answer:
-            print('Correct!')
-            score += 1
-        elif user_answer != correct_answer:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.\nLet's try again, {name}!")
-            break
-        if score == finish:
-            print(f'Congratulations, {name}!')
+    run_game(even)
 
 if __name__ == '__main__':
     main()
