832db07995c666f8cb9a319d0a8315805e3b377c
motlakhov <dmitrijmotlahov@gmail.com>
2023-11-23 18:43:25 +0300

    refactoring

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,12 +4,10 @@ RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(number):
-    prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
-                     43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-    if number in prime_numbers:
-        return True
-    elif number not in prime_numbers:
-        return False
+    d = 2
+    while number % d != 0:
+        d += 1
+    return d == number
 
 
 def task_func():
--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,19 +3,16 @@ from random import randint, choice
 RULES = 'What number is missing in the progression?'
 
 
-def progression():
-    start_num = randint(1, 20)
-    step = randint(1, 5)
-    list_progression = [start_num + step * i for i in range(10)]
+def progression(start_num, step, num_terms):
+    list_progression = [start_num + step * i for i in range(num_terms)]
     return list_progression
 
 
-def question_string():
-    progression_variable = progression()
-    missing_element_int = choice(progression_variable)
+def question_string(progression):
+    missing_element_int = choice(progression)
     missing_element = str(missing_element_int)
     modified_progression = ''
-    for num in progression_variable:
+    for num in progression:
         if num != missing_element_int:
             modified_progression = modified_progression + str(num) + ' '
         else:
@@ -24,7 +21,8 @@ def question_string():
 
 
 def task_func():
-    modified_progression, missing_element = question_string()
+    list_progression = progression(randint(1, 20), randint(1, 5), 10)
+    modified_progression, missing_element = question_string(list_progression)
     question = modified_progression
     correct_answer = missing_element
     return question, correct_answer
