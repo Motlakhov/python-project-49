d59e9a484029313b5d2b1fd8246b3cf65153a1e8
motlakhov <dmitrijmotlahov@gmail.com>
2023-11-28 15:07:36 +0300

    refactoring

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,11 +1,15 @@
 from random import randint, choice
 
 RULES = 'What number is missing in the progression?'
+MIN_START_NUM = 1
+MAX_START_NUM = 20
+MIN_STEP = 2
+MAX_STEP = 10
 
 
-def progression(start_num, step, num_terms):
-    list_progression = [start_num + step * i for i in range(num_terms)]
-    return list_progression
+def calculate_progression(start_num, step, num_terms):
+    progression = [start_num + step * i for i in range(num_terms)]
+    return progression
 
 
 def question_string(progression):
@@ -17,12 +21,13 @@ def question_string(progression):
             modified_progression = modified_progression + str(num) + ' '
         else:
             modified_progression += '.. '
-    return modified_progression, missing_element
+    return modified_progression, str(missing_element)
 
 
 def task_func():
-    list_progression = progression(randint(1, 20), randint(1, 5), 10)
-    modified_progression, missing_element = question_string(list_progression)
-    question = modified_progression
-    correct_answer = missing_element
+    start_num = randint(MIN_START_NUM, MAX_START_NUM)
+    step = randint(MIN_STEP, MAX_STEP)
+    num_terms = 10
+    progression_result = calculate_progression(start_num, step, num_terms)
+    question, correct_answer = question_string(progression_result)
     return question, correct_answer
